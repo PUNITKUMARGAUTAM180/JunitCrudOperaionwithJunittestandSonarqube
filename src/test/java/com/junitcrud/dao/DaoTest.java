099f1c335968cea8874b556cb0932b0ea2a3a805
PUNITKUMARGAUTAM <punitgautam180@gmail.com>
2022-11-14 10:27:34 +0530

    first commit

--- src/test/java/com/junitcrud/dao/DaoTest.java
+++ src/test/java/com/junitcrud/dao/DaoTest.java
@@ -1,54 +1,54 @@
-//package com.junitcrud.dao;
-//
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.Mockito.when;
-//import java.util.ArrayList;
-//import java.util.List;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mock;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import com.junitcrud.entity.PersonEntity;
-//@SpringBootTest
-//public class DaoTest {
-//
-//    @Mock
-//    PersonDao personDao;
-//
-//    @Test
-//    public void getById(){
-//        PersonEntity personEntity=new PersonEntity(5,"Punit Kumar","Varanasi","Gautam");
-//        personDao.findById(personEntity.getId());
-//
-//    }
-//
-//    @Test
-//    public void findAllTest(){
-//        List<PersonEntity> personEntityList=new ArrayList<>();
-//        when(personDao.findAll()).thenReturn(personEntityList);
-//
-//    }
-//
-//    @Test
-//    public void deleteByIdTest() {
-//        PersonEntity personEntity=new PersonEntity();
-//        personEntity.setId(8);
-//        personDao.deleteById(personEntity.getId());
-//
-//
-//    }
-//
-//    @Test
-//    public void getByIdTest(){
-//        PersonEntity personEntity=new PersonEntity();
-//        personEntity.setId(10);
-//        when(personDao.findById(personEntity.getId())).thenReturn(any());
-//
-//    }
-//    @Test
-//    public void updateByIdTest(){
-//        PersonEntity personEntity=new PersonEntity(11,"Bharat","Sio","Yadav");
-//        when(personDao.save(personEntity)).thenReturn(any());
-//
-//
-//    }
-//}
+package com.junitcrud.dao;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.junitcrud.entity.PersonEntity;
+@SpringBootTest
+public class DaoTest {
+
+    @Mock
+    PersonDao personDao;
+
+    @Test
+    public void getById(){
+        PersonEntity personEntity=new PersonEntity(5,"Punit Kumar","Varanasi","Gautam");
+        personDao.findById(personEntity.getId());
+
+    }
+
+    @Test
+    public void findAllTest(){
+        List<PersonEntity> personEntityList=new ArrayList<>();
+        when(personDao.findAll()).thenReturn(personEntityList);
+
+    }
+
+    @Test
+    public void deleteByIdTest() {
+        PersonEntity personEntity=new PersonEntity();
+        personEntity.setId(8);
+        personDao.deleteById(personEntity.getId());
+
+
+    }
+
+    @Test
+    public void getByIdTest(){
+        PersonEntity personEntity=new PersonEntity();
+        personEntity.setId(10);
+        when(personDao.findById(personEntity.getId())).thenReturn(any());
+
+    }
+    @Test
+    public void updateByIdTest(){
+        PersonEntity personEntity=new PersonEntity(11,"Bharat","Sio","Yadav");
+        when(personDao.save(personEntity)).thenReturn(any());
+
+
+    }
+}
--- src/test/java/com/junitcrud/service/ServiceTest.java
+++ src/test/java/com/junitcrud/service/ServiceTest.java
@@ -1,61 +1,61 @@
-//package com.junitcrud.service;
-//
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.Mockito.when;
-//import java.util.ArrayList;
-//import java.util.List;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mock;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import com.junitcrud.dao.PersonDao;
-//import com.junitcrud.entity.PersonEntity;
-//
-//@SpringBootTest
-//public class ServiceTest {
-//
-//    @Mock
-//    private PersonDao personDao;
-//    @Autowired
-//    private PersonService personService;
-//
-//    @Test
-//    public void addPeopleTest(){
-//        PersonEntity personEntity=new PersonEntity(1,"Amitkumar","Dinapurvaranasi","GautamSingh");
-//        when(personDao.save(personEntity)).thenReturn(any());
-//        personService.addPeople(personEntity);
-//
-//    }
-//
-//    @Test
-//    public void updateByIdTest(){
-//        PersonEntity personEntity=new PersonEntity(6,"Amit","Dinapur","Gautam");
-//        when(personDao.save(personEntity)).thenReturn(any());
-//        personService.updateById(personEntity);
-//
-//    }
-//    @Test
-//    public void getByIdTest(){
-//        PersonEntity personEntity=new PersonEntity();
-//        personEntity.setId(1);
-//        when(personDao.findById(personEntity.getId())).thenReturn(any());
-//        personService.getById(personEntity.getId());
-//    }
-//
-//    @Test()
-//    public void findAllTest(){
-//        List<PersonEntity> personEntityList=new ArrayList<>();
-//        when(personDao.findAll()).thenReturn(personEntityList);
-//        personService.findAll();
-//
-//    }
-//
-//    @Test
-//    public void deleteByIdTest() {
-//        PersonEntity personEntity=new PersonEntity();
-//        personEntity.setId(3);
-//        personDao.deleteById(personEntity.getId());
-//        personService.deleteById(personEntity.getId());
-//
-//    }
-//}
+package com.junitcrud.service;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.junitcrud.dao.PersonDao;
+import com.junitcrud.entity.PersonEntity;
+
+@SpringBootTest
+public class ServiceTest {
+
+    @Mock
+    private PersonDao personDao;
+    @Autowired
+    private PersonService personService;
+
+    @Test
+    public void addPeopleTest(){
+        PersonEntity personEntity=new PersonEntity(1,"Amitkumar","Dinapurvaranasi","GautamSingh");
+        when(personDao.save(personEntity)).thenReturn(any());
+        personService.addPeople(personEntity);
+
+    }
+
+    @Test
+    public void updateByIdTest(){
+        PersonEntity personEntity=new PersonEntity(6,"Amit","Dinapur","Gautam");
+        when(personDao.save(personEntity)).thenReturn(any());
+        personService.updateById(personEntity);
+
+    }
+    @Test
+    public void getByIdTest(){
+        PersonEntity personEntity=new PersonEntity();
+        personEntity.setId(1);
+        when(personDao.findById(personEntity.getId())).thenReturn(any());
+        personService.getById(personEntity.getId());
+    }
+
+    @Test()
+    public void findAllTest(){
+        List<PersonEntity> personEntityList=new ArrayList<>();
+        when(personDao.findAll()).thenReturn(personEntityList);
+        personService.findAll();
+
+    }
+
+    @Test
+    public void deleteByIdTest() {
+        PersonEntity personEntity=new PersonEntity();
+        personEntity.setId(3);
+        personDao.deleteById(personEntity.getId());
+        personService.deleteById(personEntity.getId());
+
+    }
+}
